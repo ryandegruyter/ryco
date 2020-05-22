fbe4945ef37a459fcf4d39c6a2c454682837ce1b
Ryan de Gruyter <ryan.degruyter@kbc.be>
2020-05-23 01:55:02 +0200

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'ryco'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ryco');
+    expect(app.title).toEqual('ryco dev');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ryco app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ryco dev app is running!');
   });
 });
